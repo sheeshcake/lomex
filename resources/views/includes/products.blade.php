1170a06e6cf251c0d6151507a10e67fd7c2e5185
wendale1231 <spooderm5@gmail.com>
2021-01-29 13:39:46 +0800

    update products

--- resources/views/includes/products.blade.php
+++ resources/views/includes/products.blade.php
@@ -70,6 +70,22 @@
                         </button>
                      </div>
                 </div>
+                <div class="card m-4 p-2 products">
+                    <h6 class="card-title text-danger">Dunlop Grandtrek® AT20™</h6>
+                    <b class="card-subtitle mb-2 text-muted">All-Season | 60K Mi. Tread Life Ltd. Warranty</b>
+                    <hr>
+                    <div class="card-product d-flex mb-3" >
+                        <img src="/img/products/dunlop-grandtrek-at20.png" alt="" class="product-image">
+                        <p class="product-desc">Drive your luxury SUV in quiet comfort.</p>
+                    </div>
+                    <hr>
+                    <div class="d-flex justify-content-center">
+                        <button class="btn btn-primary" style="width: 80%;">
+                            View Details
+                            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                        </button>
+                     </div>
+                </div>
             </div>
             <div class="d-flex justify-content-center">    
                 <button class="btn btn-primary" style="width: 80%;">
